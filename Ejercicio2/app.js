894518f19db72abb69d26a777ae1f2c1110cabb8
arthurarj1000 <arturoarjim1000@gmail.com>
2023-11-09 22:54:40 -0600

    Ejercicio 2 completado

---
+++ Ejercicio2/app.js
@@ -0,0 +1,38 @@
+// // EJERCICIO 1
+// // Crear un array vacío
+// const numerosAleatorios = [];
+
+// // Generar 10 números aleatorios y guardarlos en el array
+// for (let i = 0; i < 10; i++) {
+//     const numeroAleatorio = Math.floor(Math.random() * 100); // Genera números aleatorios entre 0 y 99
+//     numerosAleatorios.push(numeroAleatorio);
+// }
+// // Mostrar el array resultante en la consola
+// console.log(numerosAleatorios);
+
+// // EJERCICIO 2
+// // Solicitar al usuario que ingrese un string con palabras separadas por comas
+// const words= prompt("Ingresa lo que quieras, pero el lugar de usar espacio para separarlas, usa comas.");
+
+// // Dividir el string en un array utilizando la coma como separador
+// const wordsArray= words.split(",");
+
+// Mostrar el array resultante en la consola
+// console.log(wordsArray);
+
+// EJERCICIO 3
+// const numeros = [10, 40, 30, 20, 15, 5];
+
+// // Ordenar el arreglo de menor a mayor
+// const numerosOrdenados = numeros.slice().sort((a, b) => a - b);
+
+// // Obtener el número menor (el primer elemento del arreglo ordenado)
+// const numeroMenor = numerosOrdenados[0];
+
+// // Obtener el número mayor (el último elemento del arreglo ordenado)
+// const numeroMayor = numerosOrdenados[numerosOrdenados.length - 1];
+
+// // Mostrar el arreglo ordenado, el número menor y el número mayor en la consola
+// console.log("El arreglo ordenado de menor a mayor:", numerosOrdenados);
+// console.log("El número menor es:", numeroMenor);
+// console.log("El número mayor es:", numeroMayor);
