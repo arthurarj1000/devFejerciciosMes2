279868dcf841e303018277a17f92e73b2a5160c8
arthurarj1000 <arturoarjim1000@gmail.com>
2024-05-13 15:26:40 -0600

    Kata 1 clase ejercicios

--- ejercicios en kata/app.js
+++ ejercicios en kata/app.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+//Kata 1
+//const persona1= prompt ("Dime un número guapo");
+//const multiDeLosDos = persona1 * 2;
+//console.log(multiDeLosDos);
+
+//const request1= prompt("Dame un nùmero papi");
+//const request2= prompt("Dame un número babé");
+//const resta= request1 - request2;
+//console.log(resta);
+
+//const r= 5.3;
+//const pi= 3.14159265359
+//const square = r**2;
+//const operation= pi*square;
+//const radio= operation;  
+//console.log(radio);
+
+//const p= 5;
+//const D= 8;
+//const d=6;
+//const operation= D*d/2;
+//const area= operation;
+//console.log(area);
+
+//Kata 2
