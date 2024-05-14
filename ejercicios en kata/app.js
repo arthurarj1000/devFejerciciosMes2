1e44a53af9891155407301a39fa387be1392845e
arthurarj1000 <arturoarjim1000@gmail.com>
2024-05-13 21:21:34 -0600

    IF ELSE IF ELSE ejercicios

--- ejercicios en kata/app.js
+++ ejercicios en kata/app.js
@@ -1,4 +1,4 @@
-//Kata 1
+//Kata 1 Aprender los tipos de variables, operadores, etc..
 //const persona1= prompt ("Dime un número guapo");
 //const multiDeLosDos = persona1 * 2;
 //console.log(multiDeLosDos);
@@ -22,4 +22,48 @@
 //const area= operation;
 //console.log(area);
 
-//Kata 2
+//Kata 2 CONDICIONALES
+//let age= 10;
+//if(age >= 18) console.log("Eres mayor de edad");
+//console.log("Fin!");
+
+//let age=20;
+//if(age>=18){
+//    console.log(`Tienes `+age+ ` años`);
+//    console.log(`Eres mayor de edad`);
+//}
+//console.log(`fin!`);
+
+//let age=20;
+//if(age >=18){
+//    console.log('Eres mayo de edad');
+//}else{
+//    console.log('Eres menor de edad');
+//}
+
+//let light = 'rojo';
+//if(light === 'rojo'){
+//    console.log('No avances');
+//}else if(light === 'amarillo'){
+//    console.log('Baja la velocidad');
+//}else if(light==='verde'){
+//    console.log("Avanza!");
+//}else{
+//    console.log('Ese no es un color válido');
+//}
+
+//let number= 522;
+//let operation= number%2;
+//if(operation === 0){
+//    console.log('El número es par');
+//}else {
+//    console.log('El número es impar');
+//}
+
+//const numberInput = prompt('Dame un número');
+//const number = parseInt(numberInput);
+//if (number % 3 === 0){
+//    console.log(`El número ${number} es divisible entre 3 y el resultado es ${number/3}.`)
+//}else{
+//    console.log(`El número ${number} no es divisible entre 3`)
+//}
