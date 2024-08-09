a81b43cb890516e4cb731ac57d9fce3db3e73525
arthurarj1000 <arturoarjim1000@gmail.com>
2024-08-08 19:41:25 -0600

    Random Number game

--- ejercicios en kata/Ejemplo de utilizar Bootstrap/app.js
+++ ejercicios en kata/Ejemplo de utilizar Bootstrap/app.js
@@ -4,18 +4,14 @@ const button = document.getElementById('check-button');
 const hint = document.getElementById('hint');
 const scoreContainer = document.getElementById('score');
 
-let score = 100;
+let score = 0;
 scoreContainer.textContent = score;
-
-
-
 
 
 const compareToRandom= function (){
         const randomNumber = Math.trunc(Math.random()*21);
         secretNumberContainer.textContent = `El número secreto fue: ${randomNumber}`;
         const respuesta = Number(input.value);
-        
         if (respuesta===randomNumber){
             hint.textContent='Correcto!';
             hint.classList = 'alert alert-success';
@@ -24,13 +20,9 @@ const compareToRandom= function (){
         }else if (respuesta > randomNumber){
             hint.textContent='Intenta un número más pequeño';
             hint.classList = 'alert alert-danger';
-            score--;
-            scoreContainer.textContent = score;
         }else{
             hint.textContent='Intenta con un número más grande, sin miedo al éxito';
             hint.classList = 'alert alert-danger';
-            score--;
-            scoreContainer.textContent = score;
         }
         button.textContent='Volver a jugar';
 }
